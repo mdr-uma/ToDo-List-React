abe0b92dcefc3f210b64ade3a038e97d94f61133
github username <um_2003_8@hotmail.com>
2020-08-05 19:02:33 -0400

    created App class and rendered form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return(
+      <div className="container">
+        <header>
+          <h2>ToDo List</h2>
+          <form id="to-do-form">
+            <input type="text" placeholder="Enter Text"/>
+            <button type="submit">Add Item</button>
+          </form>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
